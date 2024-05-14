13fb64a068134039226bb43c314b7ace97253357
Ok-ss <149368635+Ok-ss@users.noreply.github.com>
2024-05-14 13:43:57 +0300

    Update coloring.py

--- coloring/coloring.py
+++ coloring/coloring.py
@@ -16,23 +16,25 @@ def assign_numbers_to_nodes(OG):
 def add_color(G, node, color):
     if color not in G.nodes[node]['colors']:
         G.nodes[node]['colors'].append(color)
-        remove_color(G, -node, color)
+        remove_color(G, -node, color) #now that it`s in the -n, it can`t be in n
 
 def remove_color(G, node, color):
     if color in G.nodes[node]['colors']:
         G.nodes[node]['colors'].remove(color)
-        if len(G.nodes[node]['colors']) == 0:
-            raise ValueError('Graph is not colorable')
+        if len(G.nodes[node]['colors']) == 0: 
+            raise ValueError('Graph is not colorable') #if there is already no color in the node, something went wrong
         if len(G.nodes[node]['colors']) == 1:
             for neigh in G[node]:
-                add_color(G, neigh, G.nodes[node]['colors'][0])
-
+                #now that this node has only one possible color, we can label it as "forbidden" for all the neighbouring ones
+                add_color(G, neigh, G.nodes[node]['colors'][0]) 
 def create_implementation_graph(OG:nx.Graph):
     Imp_G = nx.DiGraph() 
-    nodes = list(OG.nodes) + [-a for a in OG.nodes]
+    nodes = list(OG.nodes) + [-a for a in OG.nodes] # nodes and their negations
     Imp_G.add_nodes_from(nodes)
+    #if 1 is connected to 2, eather one of them will have a spesific color or neither. 
+    # -1 | -2 => (1 -> -2)(2 -> -1)
     for edge in OG.edges:
-        Imp_G.add_edge(edge[0], -edge[1])
+        Imp_G.add_edge(edge[0], -edge[1]) 
         Imp_G.add_edge(edge[1], -edge[0])
 
     for node in Imp_G.nodes:
@@ -54,11 +56,12 @@ def choose_colors(Imp_G: nx.Graph):
                 possible_colors = []
                 for neigh in Imp_G[node]:
                     possible_colors += list(Imp_G.nodes[neigh]['colors'])
-                opt_1, opt_2 = tuple(Imp_G.nodes[node]['colors'])
+                opt_1, opt_2 = tuple(Imp_G.nodes[node]['colors']) #colors that you cold assign to a node
                 
                 opt_1_count = possible_colors.count(opt_1)
                 opt_2_count = possible_colors.count(opt_2)
 
+                #we choose the one that appears more frequently in the neighbouring "forbidden" nodes, so we satisfy more of them
                 if opt_1_count < opt_2_count:
                     remove_color(Imp_G, node, opt_1)
                 elif opt_1_count > opt_2_count:
